c5d2b8d5498f85e324f55a7aec3670483b27aafa
abhisheksingh <abhisheksingh.geek123@gmail.com>
2023-11-29 20:00:04 +0530

    topHeader component added

---
+++ src/app/top-header/top-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'top-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-header.component.html',
+  styleUrl: './top-header.component.css'
+})
+export class TopHeaderComponent {
+
+}
