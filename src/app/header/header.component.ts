a615a3246ff888b2ec8f04daf2f434bbf5a09d07
abhisheksingh <abhisheksingh.geek123@gmail.com>
2023-11-30 15:20:05 +0530

    top menu and main menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TopMenuComponent, MainMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
