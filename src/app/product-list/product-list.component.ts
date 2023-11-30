4e1cbfe39a9c73ad983be703e9800274729fb421
abhisheksingh <abhisheksingh.geek123@gmail.com>
2023-11-30 23:18:54 +0530

    property binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,16 +9,22 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  name = "John Doe";
   product = {
     name: 'iPhone 15',
     price: 999,
     color: 'Black',
     discount: 8.5,
-    inStock: 5
+    inStock: 5,
+    productImage: '/assets/images/iphone.png'
   }
   
   getDiscountedPrice(){
     return this.product.price - (this.product.discount * this.product.price /100)
+  }
+
+  isInStock(){
+    return this.product.inStock > 0 ? false : true;
   }
 
 }
