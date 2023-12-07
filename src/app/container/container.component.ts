b45182a17b0b898aedc9e268f92207dbcf4f8c68
abhisheksingh <abhisheksingh.geek123@gmail.com>
2023-12-07 16:03:17 +0530

    rendering list of Complex type

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
-  selector: 'product-list',
+  selector: 'app-container',
   standalone: true,
-  imports: [CommonModule, SearchComponent],
+  imports: [CommonModule, SearchComponent, ProductListComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
